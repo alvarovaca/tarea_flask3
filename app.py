59b5c2cf6f64e7d9eccaf899a21df7946136c96d
Álvaro Vaca Ferreras <avacaferreras@gmail.com>
2020-04-23 17:11:08 +0200

    Inserción base.html.

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+import json
+#import os
+app = Flask(__name__)
+
+with open("msx.json") as fichero:
+    datos=json.load(fichero)
+
+@app.route('/',methods=["GET"])
+def inicio():
+	return render_template("inicio.html")
+
+app.run(debug=True)
+#port=os.environ["PORT"]
+#app.run('0.0.0.0', int(port), debug=False)
